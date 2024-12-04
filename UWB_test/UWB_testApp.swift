2e3976001d3076fef775a3577bc67e37ef3e8470
SArthurX <sxa05z@gmail.com>
2024-12-04 22:34:39 +0800

    Initial Commit

---
+++ UWB_test/UWB_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  UWB_testApp.swift
+//  UWB_test
+//
+//  Created by 陳冠言 on 2024/12/4.
+//
+
+import SwiftUI
+
+@main
+struct UWB_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UWB_testTests/UWB_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  UWB_testTests.swift
+//  UWB_testTests
+//
+//  Created by 陳冠言 on 2024/12/4.
+//
+
+import Testing
+@testable import UWB_test
+
+struct UWB_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
